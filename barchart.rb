6164437b3a097ee842fac501ca4d3205258220f4
Fronx <fronx@soundcloud.com>
2012-03-28 18:30:29 +0200

    dunno what that is

--- barchart.rb
+++ barchart.rb
@@ -1,4 +1,6 @@
-# FORMAT:
+#! /usr/bin/env ruby
+#
+# INPUT FORMAT:
 #   <graph-name>.<key> <value>
 # OR:
 #   <key> <value>
@@ -7,7 +9,7 @@ Row = Struct.new(:ns, :key, :value)
 
 rows = STDIN.map do |row|
   key, value = row.split(' ')
-  ns, key = key.split('.')
+  ns, key = key.split('.')[0], key.split('.')[1..-1].join('.')
   ns, key = nil, ns if !key
   Row.new(ns, key, value.to_i)
 end
@@ -18,10 +20,10 @@ rows.group_by { |r| r.ns }.each do |ns, rows|
   value_characters = 80 - max_length
   scale = [(max_value.to_f / value_characters).ceil, 1].max
 
-  ns && puts("# #{ns.upcase}")
+  ns && puts("# #{ns}")
   (scale > 1) && puts("# each * represents a count of %d" % scale)
 
-  format = "%#{max_length}s [%#{max_value.to_s.length}d] %s"
+  format = "%-#{max_length}s [%#{max_value.to_s.length}d] %s"
   rows.each do |row|
     puts format % [ row.key, row.value, "*" * (row.value / scale) ]
   end
